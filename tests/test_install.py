3efcf62c00f88c54c27184ab0fa00e6101933ed5
balbasty <yael.balbastre@gmail.com>
2025-03-17 21:05:29 +0000

    [WIP]

--- tests/test_install.py
+++ tests/test_install.py
@@ -3,3 +3,7 @@ from matlab_runtime_installer import install
 
 def test_install_r2024b():
     install("R2024b", auto_answer=True)
+
+
+def test_install_r2024a():
+    install("R2024a", auto_answer=True)
